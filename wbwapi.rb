6b5443d3429ce4d722153a40f78365c3ff2ecae2
Jaap Frolich <jfrolich@gmail.com>
2012-04-10 17:01:41 +0200

    Spaces...

--- wbwapi.rb
+++ wbwapi.rb
@@ -18,34 +18,34 @@ end
 
 get '/login' do
   begin
-		@wbw_client.login(params[:username], params[:password])
-		{}.to_json
-	rescue Wbw::Exceptions::Exception => e
-		return_error e
-	end
+    @wbw_client.login(params[:username], params[:password])
+    {}.to_json
+  rescue Wbw::Exceptions::Exception => e
+    return_error e
+  end
 end
 
 get '/logout' do
-	@wbw_client.logout
-	{}.to_json
+  @wbw_client.logout
+  {}.to_json
 end
 
 get '/lists' do
   begin
-		@wbw_client.lists.to_json
-	rescue Wbw::Exceptions::Exception => e
-		return_error e
-	end
+    @wbw_client.lists.to_json
+  rescue Wbw::Exceptions::Exception => e
+    return_error e
+  end
 end
 
 get '/payments/:lid' do
-	begin
-		@wbw_client.payments(params[:lid]).to_json
-	rescue Wbw::Exceptions::Exception => e
-		return_error e
+  begin
+    @wbw_client.payments(params[:lid]).to_json
+  rescue Wbw::Exceptions::Exception => e
+    return_error e
   end
 end
 
 def return_error e
-	[e.response_code, {error_message: e.message}.to_json]
+  [e.response_code, {error_message: e.message}.to_json]
 end
